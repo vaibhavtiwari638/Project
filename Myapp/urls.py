c5be428bf31152275fbda413d522ba4424bdd691
vaibhavtiwari638 <156024215+vaibhavtiwari638@users.noreply.github.com>
2024-01-09 12:03:22 +0530

    Update urls.py

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -3,8 +3,6 @@ from unicodedata import name
 from django.urls import path
 from Myapp.views import *
 urlpatterns=[
-    path('home',homepage,name='home'),
-    path('Check',Predict,name='Check'),
     path('login',login,name='login'),
     path('signin',fun1,name="login"),
     path('signup',fun2,name="login"),
